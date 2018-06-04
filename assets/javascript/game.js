8553c1565c8b2d6694c0ab1eaa13349d35cee3d8
Frank Santiesteban <franksantiesteban@nancy-vaio.atnind.local>
2018-06-04 16:35:29 -0400

    added vars, math.random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,20 @@ var win;
 var losses; 
 var randomResults;
 
+randomResults = Math.floor(Math.random() * 69) + 30;
+
+$("#result").html('Random Result: ')
+
 for (var i = 0; i < 4; i++){
+    var random = Math.floor(Math.random()* 12);
+
+
     var crystal = $("<div>");
-        crystal.attr("class", 'crystal');
-        
+        crystal.attr({
+        "class": 'crystal',
+        "data-random": random
+    });
+
     $(".crystals").append(crystal);
 
 }
