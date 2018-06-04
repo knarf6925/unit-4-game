8fb66b99d9b2559a71e69543a8acabb0d3ab47a8
Frank Santiesteban <franksantiesteban@Franks-MacBook-Pro.local>
2018-06-03 20:23:23 -0400

    made divs for crystals

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var win;
+var losses; 
+var randomResults;
+
+for (var i = 0; i < 4; i++){
+    var crystal = $("<div>");
+        crystal.attr("class", 'crystal');
+        
+    $(".crystals").append(crystal);
+
+}
